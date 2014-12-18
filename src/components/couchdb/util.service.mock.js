4753417654478bd083cb72178ed08b73a2f4dcb2
Tom Vincent <git@tlvince.com>
2014-12-18 15:02:53 +0000

    Add couchUtil pluck helper specs

---
+++ src/components/couchdb/util.service.mock.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('couchUtilMock', [])
+  .constant('pluckResponseMock', {
+    rows: [
+      {
+        key: 'key1',
+        id: 'id1',
+        value: 'value1',
+        doc: {
+          _id: 'doc1'
+        }
+      },
+      {
+        key: 'key2',
+        id: 'id2',
+        value: 'value2',
+        doc: {
+          _id: 'doc2'
+        }
+      }
+    ]
+  });

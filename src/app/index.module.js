0a59ea3612c44dce3fc8ac5453e1ada4e35c19d0
Haykel Ben Jemia <hbj@allmas.tn>
2014-12-28 19:28:40 +0100

    Redirect anonymous users to login page.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,15 +1,23 @@
 'use strict';
 
 angular.module('directDelivery', [
-  'core',
-  'navbar',
-  'footer',
-  'home',
-  'auth',
-  'log',
-  'login',
-  'scheduler',
-  'packing',
-  'packing.all',
-  'packing.item'
-]);
+    'core',
+    'navbar',
+    'footer',
+    'home',
+    'auth',
+    'log',
+    'login',
+    'scheduler',
+    'packing',
+    'packing.all',
+    'packing.item'
+  ])
+  .run(function($rootScope, $state, AuthService) {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+      if (!AuthService.currentUser() && toState.name !== 'login') {
+        $state.transitionTo('login');
+        event.preventDefault();
+      }
+    });
+  });

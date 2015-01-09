46cb927013a7028f11c06f39190b414d2df53b7a
Tom Vincent <git@tlvince.com>
2015-01-09 09:16:11 +0000

    Set packing.all as default packing state

--- src/app/packing/all/all.states.js
+++ src/app/packing/all/all.states.js
@@ -3,6 +3,7 @@
 angular.module('packing.all')
   .config(function($stateProvider) {
     $stateProvider.state('packing.all', {
+      url: '',
       templateUrl: 'app/packing/all/all.html',
       controller: 'PackingAllCtrl',
       controllerAs: 'packingAllCtrl'

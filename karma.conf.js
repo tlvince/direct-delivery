0918689726b5aeebbf64736f052ae026984af48c
Tom Vincent <git@tlvince.com>
2015-01-09 11:39:48 +0000

    Reintroduce karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,27 @@
 'use strict';
+/*eslint-env node */
 
 module.exports = function(config) {
-
   config.set({
-    autoWatch : false,
-
-    frameworks: ['jasmine'],
-
-    browsers : ['PhantomJS'],
-
-    plugins : [
-        'karma-phantomjs-launcher',
-        'karma-jasmine'
-    ]
+    autoWatch: false,
+    frameworks: [
+      'jasmine'
+    ],
+    browsers: [
+      'PhantomJS'
+    ],
+    reporters: [
+      'progress',
+      'coverage'
+    ],
+    preprocessors: {
+      'src/{app,components}/**/!(*.spec|*.mock).js': [
+        'coverage'
+      ]
+    },
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage'
+    }
   });
 };

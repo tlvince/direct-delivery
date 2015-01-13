e8536475b6057a297fce4929a9e10e7c631de1cb
Tom Vincent <git@tlvince.com>
2015-01-13 14:03:16 +0000

    Add empty response legend mock

--- src/app/packing/item/legend.service.mock.js
+++ src/app/packing/item/legend.service.mock.js
@@ -1,27 +1,14 @@
 'use strict';
 
-
 angular.module('packingItemLegendServiceMock', [])
-  .service('couchdb', function() {
-    var normalResponse = {
-      rows: [
-        {
-          doc: {
-            _id: 'product-storage/dry',
-            label: 'label',
-            description: 'description'
-          }
-        }
-      ]
-    };
-
+  .service('couchdb', function(PRODUCT_STORAGE_MOCK) {
     this.view = function() {
       return {
         $promise: {
           then: function(pluck) {
             return {
               then: function(format) {
-                return format(pluck(normalResponse));
+                return format(pluck(PRODUCT_STORAGE_MOCK));
               }
             };
           }
--- src/app/packing/item/legend.service.spec.js
+++ src/app/packing/item/legend.service.spec.js
@@ -3,8 +3,12 @@
 /*global module: false, inject: false */
 
 describe('packingItemLegendService', function() {
-  describe('normal response', function() {
-    beforeEach(module('packing.item', 'packingItemLegendServiceMock'));
+  describe('expected response', function() {
+    beforeEach(module(
+      'packing.item',
+      'productStorageMockExpected',
+      'packingItemLegendServiceMock'
+    ));
 
     var packingItemLegendService;
     beforeEach(inject(function(_packingItemLegendService_) {
@@ -19,6 +23,32 @@ describe('packingItemLegendService', function() {
           label: 'label',
           description: 'description',
           bsClass: 'label-active'
+        }
+      ];
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe('unexpected response', function() {
+    beforeEach(module(
+      'packing.item',
+      'productStorageMockUnexpected',
+      'packingItemLegendServiceMock'
+    ));
+
+    var packingItemLegendService;
+    beforeEach(inject(function(_packingItemLegendService_) {
+      packingItemLegendService = _packingItemLegendService_;
+    }));
+
+    it('should return an empty bsClass', function() {
+      var actual = packingItemLegendService.get();
+      var expected = [
+        {
+          id: undefined,
+          label: undefined,
+          description: undefined,
+          bsClass: ''
         }
       ];
       expect(actual).toEqual(expected);

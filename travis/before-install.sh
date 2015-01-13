67b54cbf4123f68bab459ebf2844c42f252c1b55
Tom Vincent <git@tlvince.com>
2015-01-13 18:32:43 +0000

    Disable Travis on pull requests
    Mark pull requests builds as errored.

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -6,4 +6,4 @@ error() { echo "$0: $1"; exit 1; }
 [[ "$TRAVIS" ]] || error "this script assumes it's running within TravisCI"
 [[ "$TRAVIS_PULL_REQUEST" == "false" ]] || error "pull request builds disabled"
 
-npm test
+echo "machine github.com login $CI_USER_TOKEN" >> ~/.netrc

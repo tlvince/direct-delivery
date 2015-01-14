2da9fea6cbcb9b116730818986b277ae3852ad78
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-14 09:20:47 +0100

    update constants refs item:1019

--- src/components/delivery/delivery-steps.constant.js
+++ src/components/delivery/delivery-steps.constant.js
@@ -1,9 +1,11 @@
 angular.module('delivery')
   .constant('DELIVERY_STEPS', {
-    START: 1,
-    DELIVER_ITEM: 2,
+    DELIVER_ITEM: 1,
+    PREVIEW_DELIVERY: 2,
     COLLECT_KPI: 3,
-    POST_ITEM_DELIVERY: 4,
+    PREVIEW_KPI: 4,
     CHILD_FACILITY: 5,
-    SIGN_OFF: 6
+    FINISH_CHILD_FACILITY: 6,
+    SIGN_OFF: 7,
+    CANCEL_DELIVERY: 8
   });

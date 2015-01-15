1ed6b60e66b18e75d03e950bfe167a6964a08e4d
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-15 10:22:47 +0100

    phew !!!  wrap signature_pad inside angular directive. refs item:1104

---
+++ src/components/signature/signature.directive.js
@@ -0,0 +1,61 @@
+angular
+  .module('signature')
+  .directive('signPad', ['$window', function ($window) {
+    var signaturePad, canvas, scope, element, EMPTY_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA=';
+
+    $window.addEventListener('resize', function () {
+      scope.$apply(function () {
+        var img = signaturePad.toDataURL();
+        signaturePad.fromDataURL(img);
+      });
+    }, false);
+
+    $window.addEventListener('orientationchange', function () {
+      scope.$apply(function () {
+        var img = signaturePad.toDataURL();
+        signaturePad.fromDataURL(img);
+      });
+    }, false);
+
+
+    return {
+      restrict: 'AE',
+      templateUrl: 'components/signature/signature-pad.html',
+      controllerAs: 'sp',
+      scope: {
+        signature: '=signature'
+      },
+      controller: function () {
+        var vm = this;
+
+        vm.clear = function(){
+          signaturePad.clear();
+        };
+
+        vm.accept = function(){
+          if (!signaturePad.isEmpty()) {
+            scope.signature.dataUrl = signaturePad.toDataURL();
+            scope.signature.$isEmpty = false;
+          } else {
+            scope.signature.dataUrl = EMPTY_IMAGE;
+            scope.signature.$isEmpty = true;
+          }
+        };
+      },
+      link: function($scope, $element){
+        canvas = $window.document.querySelector("canvas");
+        scope = $scope;
+        element = $element;
+        signaturePad = new SignaturePad(canvas);
+
+        if ($scope.signature && !$scope.signature.$isEmpty && $scope.signature.dataUrl) {
+          signaturePad.fromDataURL($scope.signature.dataUrl);
+        }
+
+        $element.on('$destroy', function(){
+          $window.off('orientationchange');
+          $window.off('resize');
+        });
+      }
+    };
+  }]);

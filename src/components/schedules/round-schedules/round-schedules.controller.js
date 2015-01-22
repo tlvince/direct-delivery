ef4e3e37010355aa767d417f6fbd1dbaf134bfd6
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-22 10:44:31 +0100

    remove unused daily schedule html files

--- src/components/schedules/round-schedules/round-schedules.controller.js
+++ src/components/schedules/round-schedules/round-schedules.controller.js
@@ -3,5 +3,9 @@
  */
 
 angular.module('schedules.round')
-  .controller('SchedulesRoundCtrl', function(scheduleService){
-  })
+  .controller('SchedulesRoundCtrl', function(scheduleService, syncService){
+
+    syncService.dailySyncDown('local', 'http://localhost:5984/deliveries',
+      'abdullahi.ahmed@example.com', '2015-01-22');
+
+  });

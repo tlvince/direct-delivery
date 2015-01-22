43378874628ea0ea28bd34328ab71b5c66d4444e
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-22 11:54:16 +0100

    changed config.DATE_FORMAT to config.dateFormat

--- src/components/utility/utility.service.js
+++ src/components/utility/utility.service.js
@@ -8,7 +8,7 @@ angular.module('utility')
   .service('utility', function ($filter, config) {
 
     this.formatDate = function(date, format){
-      var dateFormat = format || config.DATE_FORMAT;
+      var dateFormat = format || config.dateFormat;
       return $filter('date')(new Date(date), dateFormat);
     };
 

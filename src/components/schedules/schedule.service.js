c6f591a8d3879fe9367fceb3a1dbdb1c6ca837b2
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-22 10:34:18 +0100

    update to use utility service

--- src/components/schedules/schedule.service.js
+++ src/components/schedules/schedule.service.js
@@ -4,7 +4,7 @@
 'use strict';
 
 angular.module('schedules')
-  .service('scheduleService', function(user, couchdb, couchUtil){
+  .service('scheduleService', function(user, couchdb, couchUtil, utility){
 
     this.all = function() {
       var params = {
@@ -13,12 +13,13 @@ angular.module('schedules')
         reduce: false,
         include_docs: true
       };
-      var key = couchUtil.key(user.email +"-"+ moment().format("D-M-YYYY"));
+      var key = couchUtil.key(user.email +"-"+ utility.formatDate(new Date()));
+      console.log(key);
       angular.extend(params, key);
       return couchdb.view(params).$promise;
 
     };
-     
+
     this.getCurrentRound = function(){
       return this.all()
         .then(function(response){

17be024466563364c59b2aabfa9d7b62b051fc8f
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-24 12:30:04 +0100

    complete implementation of deliver items section of delivery work flow refs item:1124

---
+++ src/components/delivery/deliver-items/deliver-items.controller.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('delivery')
+  .controller('DeliverItemsCtrl', function DeliverItemsCtrl($state, deliveryService) {
+
+    var vm = this;
+
+    function init(){
+      if($state.params.preview === 'true'){
+        vm.previewDelivery = true;
+      }
+      clearValidationError();
+    }
+
+    function clearValidationError(){
+      vm.validationErr = {};
+    }
+
+    init();
+
+    vm.calcDeliverQty = function(pp){
+      var res = deliveryService.calcQty(pp);
+      pp.deliveredQty = res.deliveredQty;
+      pp.returnedQty = res.returnedQty;
+      vm.validateOnChange(pp);
+    };
+
+    vm.validateOnChange = function(pp){
+      vm.validationErr[pp.productID] = deliveryService.validateItemQty(pp);
+    };
+
+    vm.togglePreview = function(){
+      vm.previewDelivery = !vm.previewDelivery;
+    };
+
+    vm.preview = function(dItem){
+      var res = deliveryService.validateDeliverItems(dItem);
+      if(res === true){
+        vm.validationErr = {};//clear validationErr
+        vm.togglePreview();
+      }else{
+        vm.validationErr = res;
+      }
+    };
+
+  });

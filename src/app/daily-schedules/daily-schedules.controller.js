3e3875bde8ca9a47cd30727a5949dfd7b3e37013
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-24 15:03:30 +0100

    connect home daily delivery table record to delivery workflow/ module

--- src/app/daily-schedules/daily-schedules.controller.js
+++ src/app/daily-schedules/daily-schedules.controller.js
@@ -3,6 +3,16 @@
  */
 
 angular.module('schedules.daily')
-  .controller('SchedulesDailyCtrl', function(dailySchedule){
-    this.day= dailySchedule[0];
+  .controller('SchedulesDailyCtrl', function($state, dailySchedule){
+    var vm = this;
+    vm.dailyDelivery= dailySchedule[0];
+
+    vm.deliver = function(facilityId){
+      var params = {
+        ddId : vm.dailyDelivery._id,
+        facilityId: facilityId
+      };
+      $state.go('facilityDelivery.deliverItems', params);
+    };
+
   });

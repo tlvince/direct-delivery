27df630da0bff0573c313bf4c7bb58405a5b25ec
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-24 15:27:15 +0100

    updated test spec

--- src/components/delivery/delivery.controller.spec.js
+++ src/components/delivery/delivery.controller.spec.js
@@ -3,14 +3,16 @@
 /*global module: false, inject: false */
 
 describe('FacilityDeliveryCtrl', function() {
-  beforeEach(module('delivery'));
+  beforeEach(module('delivery', 'deliveryMock'));
 
   var FacilityDeliveryCtrl;
 
-  beforeEach(inject(function($controller, _$state_, DELIVERY_STEPS) {
+  beforeEach(inject(function($controller, _$state_, DELIVERY_STEPS, _deliveryService_, _dailyDeliveryMock_) {
     FacilityDeliveryCtrl = $controller('FacilityDeliveryCtrl', {
       state: _$state_,
-      DELIVERY_STEPS: DELIVERY_STEPS
+      DELIVERY_STEPS: DELIVERY_STEPS,
+      deliveryService: _deliveryService_,
+      dailyDelivery: _dailyDeliveryMock_
     });
   }));
 

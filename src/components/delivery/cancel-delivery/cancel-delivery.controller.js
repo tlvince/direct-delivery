5beb8c9167462fffd94dc2c129e3d8c7b4bc8dea
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-24 11:14:20 +0100

    completed implementation of cancel facility delivery closes item:1123

--- src/components/delivery/cancel-delivery/cancel-delivery.controller.js
+++ src/components/delivery/cancel-delivery/cancel-delivery.controller.js
@@ -1,34 +1,50 @@
 'use strict';
 
 angular.module('delivery')
-  .controller('CancelDeliveryCtrl', function CancelDeliveryCtrl($state, STATUS, deliveryService, log) {
+  .controller('CancelDeliveryCtrl', function CancelDeliveryCtrl($state, deliveryService, cancelDeliveryService,
+                                                                log, dailyDelivery) {
 
     var vm = this; //view model
-    vm.facilityName = $state.params.facilityName;
-    vm.facilityId = $state.params.facilityId;
 
-    vm.facRnd = {};
-
-    function initCancelReport(){
-      if(!vm.facRnd.cancelReport){
+    function initCancelReport() {
+      vm.dailyDelivery = dailyDelivery;
+      vm.facilityId = $state.params.facilityId;
+      var isValidFacilityId = (angular.isString(vm.facilityId) && vm.facilityId !== '');
+      if (!(angular.isObject(vm.dailyDelivery) && isValidFacilityId)) {
+        goBack();
+        return;
+      }
+      var res = deliveryService.filterByFacility(vm.dailyDelivery, vm.facilityId);
+      if (res.length > 0) {
+        vm.facRnd = res[0];
+      } else {
+        goBack();
+        return;
+      }
+      if (!vm.facRnd.cancelReport) {
         //set with default
-        vm.facRnd.cancelReport = deliveryService.getDefaultCancelReport();
+        vm.facRnd.cancelReport = cancelDeliveryService.getDefaultCancelReport();
       }
     }
 
-    function onSuccess(){
+    function goBack() {
+      log.error('facilityRoundNotSet');
+      $state.go('facilityDelivery.deliverItems', $state.params);
+    }
+
+    function onSuccess() {
       log.success('deliveryCancelled');
       $state.go('home');
     }
 
-    function onError(err){
+    function onError(err) {
       log.error(saveFailed, err);
     }
 
     initCancelReport();
 
-    vm.onReasonSelection = function(){
-      if(vm.facRnd.cancelReport.cancelledAhead === true){
+    vm.onReasonSelection = function () {
+      if (vm.facRnd.cancelReport.cancelledAhead === true) {
         //unset other reasons.
         vm.facRnd.cancelReport.others = false;
         vm.facRnd.cancelReport.hfNotAvailable = false;
@@ -37,15 +53,13 @@ angular.module('delivery')
       }
     };
 
-
     vm.cancel = function () {
-      var isValid = deliveryService.validateCancelReport(vm.facRnd);
-      if(isValid === true){
-        vm.facRnd.status = (vm.facRnd.cancelReport.cancelledAhead === true)? STATUS.CANCELLED_AHEAD : STATUS.CANCELLED;
-        deliveryService.save()
+      var isValid = cancelDeliveryService.validateCancelReport(vm.facRnd.cancelReport);
+      if (isValid === true) {
+        cancelDeliveryService.cancelDelivery(vm.dailyDelivery, vm.facRnd)
           .then(onSuccess)
           .catch(onError);
-      }else{
+      } else {
         log.error('invalidCancelReport');
       }
     };

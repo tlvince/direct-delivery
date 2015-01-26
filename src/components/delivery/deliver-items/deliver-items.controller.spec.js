05d616b8e8d647186f1bdeee3b663bbdd980f6ba
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-26 12:23:52 +0100

    fix failing deliver items ctrl test

--- src/components/delivery/deliver-items/deliver-items.controller.spec.js
+++ src/components/delivery/deliver-items/deliver-items.controller.spec.js
@@ -3,19 +3,37 @@
 /*global module: false, inject: false */
 
 describe('DeliverItemsCtrl', function() {
-  beforeEach(module('delivery'));
+  beforeEach(module('delivery', 'deliveryMock', 'log'));
 
   var DeliverItemsCtrl;
 
-  beforeEach(inject(function($controller, _$state_, _deliveryService_) {
+  beforeEach(inject(function($controller, _$state_, _dailyDeliveryMock_, _deliveryService_, _log_, FACILITY_ID) {
+
+    _$state_.params = { facilityId: FACILITY_ID };
+
+    var scope = {
+      facDevCtrl: $controller('FacilityDeliveryCtrl', {
+        state: _$state_,
+        deliveryService: _deliveryService_,
+        dailyDelivery: _dailyDeliveryMock_,
+        log: _log_
+      })
+    };
+
     DeliverItemsCtrl = $controller('DeliverItemsCtrl', {
       state: _$state_,
-      deliveryService: _deliveryService_
+      deliveryService: _deliveryService_,
+      $scope: scope
     });
+
   }));
 
   it('should expose calcDeliverQty()', function() {
     expect(DeliverItemsCtrl.calcDeliverQty).toBeDefined();
+  });
+
+  it('should expose facRnd', function(){
+    expect(DeliverItemsCtrl.facRnd.facility).toBeDefined();
   });
 
   describe('togglePreview()', function(){
@@ -25,6 +43,7 @@ describe('DeliverItemsCtrl', function() {
       var after = DeliverItemsCtrl.previewDelivery;
       expect(before).not.toBe(after)
     });
+
   });
 
 

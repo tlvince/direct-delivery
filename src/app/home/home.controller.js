e6bfe1550cf180c0cd314b88aa4c33e6c7616cd8
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-29 11:52:46 +0100

    remove listeners on scope $destroy.

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -1,25 +1,35 @@
 'use strict';
 
 angular.module('home')
-  .controller('HomeCtrl', function(dailySchedule, coreService, SYNC_STATUS, $rootScope){
+  .controller('HomeCtrl', function(dailySchedule, coreService, SYNC_STATUS, $rootScope, $scope){
     var vm = this;
+    var unbind = {};
     vm.dailyDelivery= [];
-    console.log('home controller');
-    console.log($rootScope.syncInProgress);
-    vm.syncInProgress = $rootScope.syncInProgress;
-    if(angular.isArray(dailySchedule) && dailySchedule.length > 0){
-      vm.dailyDelivery = dailySchedule[0]
+    vm.syncInProgress =coreService.getSyncInProgress();
+
+    function init(){
+      if(angular.isArray(dailySchedule) && dailySchedule.length > 0){
+        vm.dailyDelivery = dailySchedule[0]
+      }
     }
 
-    var unbind = {};
+    init();
+
     unbind[SYNC_STATUS.IN_PROGRESS] = $rootScope.$on(SYNC_STATUS.IN_PROGRESS, function(){
-      console.log('am here');
       vm.syncInProgress = coreService.getSyncInProgress();
     });
 
     unbind[SYNC_STATUS.COMPLETE] = $rootScope.$on(SYNC_STATUS.COMPLETE, function(){
-      console.log('am here 2');
       vm.syncInProgress = coreService.getSyncInProgress();
     });
+
+    function removeListeners(){
+      for(var k in SYNC_STATUS){
+        var event = SYNC_STATUS[k];
+        unbind[event]();
+      }
+    }
+
+    $scope.$on('$destroy', removeListeners);
 
   });

0df4953b5d1a4f60a26a67bf18d4c20995f8d63b
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-29 09:24:47 +0100

    add query for design docs by dd ids to pouchdb service.

--- src/components/db/db.pouchdb.service.js
+++ src/components/db/db.pouchdb.service.js
@@ -5,7 +5,9 @@
  *
  */
 angular.module('db')
-  .service('pouchdbService', function($window, pouchDB){
+  .service('pouchdbService', function($window, pouchDB, $q){
+
+    var _this = this;
 
     /**
      * we set default adapter to 'websql' because of the following:
@@ -15,7 +17,7 @@ angular.module('db')
      * @param dbName
      * @returns {*}
      */
-    this.create = function(dbName){
+    _this.create = function(dbName){
       var options = {
         adapter: 'websql',
         auto_compaction: true
@@ -28,8 +30,16 @@ angular.module('db')
       return db;
     };
 
-    this.remote = function(dbUrl, options){
+    _this.remote = function(dbUrl, options){
       return new pouchDB(dbUrl, options)
+    };
+
+    _this.getDesignDocs = function(db, ddIds){
+      var options = {
+        keys: ddIds
+      };
+      db = _this.create(db);
+      return db.allDocs(options);
     };
 
   });

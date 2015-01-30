65771c29791182a2103dcba0610a52b55a802efb
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-30 16:49:03 +0100

    refactor Login Controller to use start background sycn after login.

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,19 +1,21 @@
 'use strict';
 
 angular.module('login')
-  .controller('LoginCtrl', function($state, log, AuthService, coreService, hasCompleteDesignDocs) {
+  .controller('LoginCtrl', function($state, log, AuthService, loginService, coreService, hasCompleteDesignDocs) {
+
+    function loggedIn(){
+      log.success('authSuccess');
+      if(hasCompleteDesignDocs !== true){
+        $state.go('loadingScreen');
+      }else{
+        $state.go('home');
+      }
+      coreService.startSyncAfterLogin(AuthService.currentUser.name);
+    }
 
     this.login = function(username, password) {
-      AuthService.login(username, password)
-        .then(function() {
-          log.success('authSuccess');
-          if(hasCompleteDesignDocs !== true){
-            $state.go('loadingScreen');
-          }else{
-            $state.go('home');
-          }
-          coreService.startSyncAfterLogin(username);
-        })
+      loginService.login(username, password)
+        .then(loggedIn)
         .catch(function(err) {
           log.error(err);
         });

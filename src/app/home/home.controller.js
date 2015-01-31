7a3238440dca4e7f14589ed0fcbf0c84fc71afa7
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-01-31 12:52:06 +0100

    minor refactoring. refs item:1160

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -1,36 +1,45 @@
 'use strict';
 
 angular.module('home')
-  .controller('HomeCtrl', function(dailySchedule, coreService, SYNC_STATUS, $rootScope, $scope, HOME_TABS){
+  .controller('HomeCtrl', function(dailySchedule, coreService, AuthService, SYNC_STATUS, $rootScope, $scope, HOME_TABS){
     var vm = this;
     var unbind = {};
     vm.dailyDelivery= [];
     vm.tabs = HOME_TABS;
-    vm.syncInProgress =coreService.getSyncInProgress();
+    vm.syncInProgress = coreService.getSyncInProgress();
+
+    function addSyncListeners(){
+      unbind[SYNC_STATUS.IN_PROGRESS] = $rootScope.$on(SYNC_STATUS.IN_PROGRESS, function(){
+        vm.syncInProgress = coreService.getSyncInProgress();
+      });
+
+      unbind[SYNC_STATUS.COMPLETE] = $rootScope.$on(SYNC_STATUS.COMPLETE, function(){
+        vm.syncInProgress = coreService.getSyncInProgress();
+      });
+    }
 
     function init(){
       if(angular.isArray(dailySchedule) && dailySchedule.length > 0){
         vm.dailyDelivery = dailySchedule[0]
       }
+      addSyncListeners();
     }
 
     init();
 
-    unbind[SYNC_STATUS.IN_PROGRESS] = $rootScope.$on(SYNC_STATUS.IN_PROGRESS, function(){
-      vm.syncInProgress = coreService.getSyncInProgress();
-    });
-
-    unbind[SYNC_STATUS.COMPLETE] = $rootScope.$on(SYNC_STATUS.COMPLETE, function(){
-      vm.syncInProgress = coreService.getSyncInProgress();
-    });
-
-    function removeListeners(){
+    function removeSyncListeners(){
+      $rootScope.$digest();
       for(var k in SYNC_STATUS){
         var event = SYNC_STATUS[k];
         unbind[event]();
       }
     }
 
-    $scope.$on('$destroy', removeListeners);
+    vm.startSync = function(){
+      coreService.startSyncAfterLogin(AuthService.currentUser.name);
+      addSyncListeners();
+    };
+
+    $scope.$on('$destroy', removeSyncListeners);
 
   });

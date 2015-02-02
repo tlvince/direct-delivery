8c6270c88a307459801ff5f56112d82c36208015
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-02-02 16:52:14 +0100

    refresh home page on sync event

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -1,27 +1,25 @@
 'use strict';
 
 angular.module('home')
-  .controller('HomeCtrl', function(dailySchedule, coreService, AuthService, SYNC_STATUS, $rootScope, $scope, HOME_TABS) {
+  .controller('HomeCtrl', function(dailySchedule, coreService, AuthService, SYNC_STATUS, $rootScope, $scope, HOME_TABS, $state) {
     var vm = this;
     var unbind = {};
     vm.dailyDelivery = [];
     vm.tabs = HOME_TABS;
     vm.syncInProgress = coreService.getSyncInProgress();
 
-    function addSyncListeners(){
-      unbind[SYNC_STATUS.IN_PROGRESS] = $rootScope.$on(SYNC_STATUS.IN_PROGRESS, function() {
-        vm.syncInProgress = coreService.getSyncInProgress();
-      });
+    function processEvent(){
+      vm.syncInProgress = coreService.getSyncInProgress();
+      $state.go($state.current, $state.params, { reload: true });
+    }
 
-      unbind[SYNC_STATUS.COMPLETE] = $rootScope.$on(SYNC_STATUS.COMPLETE, function() {
-        vm.syncInProgress = coreService.getSyncInProgress();
-      });
+    function addSyncListeners(){
+      unbind[SYNC_STATUS.IN_PROGRESS] = $rootScope.$on(SYNC_STATUS.IN_PROGRESS, processEvent);
+      unbind[SYNC_STATUS.COMPLETE] = $rootScope.$on(SYNC_STATUS.COMPLETE, processEvent);
     }
 
     function init(){
-      if (angular.isArray(dailySchedule) && dailySchedule.length > 0) {
-        vm.dailyDelivery = dailySchedule[0];
-      }
+      vm.dailyDelivery = dailySchedule;
       addSyncListeners();
     }
 

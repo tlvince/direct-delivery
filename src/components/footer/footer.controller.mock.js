e9b732de225aef215c168c2dc44885a1cee826f4
Tom Vincent <git@tlvince.com>
2015-02-03 16:57:02 +0100

    Footer author spec

---
+++ src/components/footer/footer.controller.mock.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('footerCtrlMock', [])
+  .constant('config', {
+    author: 'ehealth'
+  });
--- src/components/footer/footer.controller.spec.js
+++ src/components/footer/footer.controller.spec.js
@@ -3,7 +3,7 @@
 /*global module: false, inject: false */
 
 describe('FooterCtrl', function() {
-  beforeEach(module('footer'));
+  beforeEach(module('footer', 'footerCtrlMock'));
 
   var FooterCtrl;
   beforeEach(inject(function($controller) {
@@ -13,5 +13,10 @@ describe('FooterCtrl', function() {
   it('should expose the current year', function() {
     var actual = FooterCtrl.year;
     expect(actual).toMatch(/^201/);
+  });
+
+  it('should expose config author', function() {
+    var actual = FooterCtrl.author;
+    expect(actual).toBe('ehealth');
   });
 });

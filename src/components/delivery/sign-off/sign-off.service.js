e3f61c3549a0114a343cae59ecc1397316b214af
Tom Vincent <git@tlvince.com>
2015-02-12 16:58:48 +0100

    Bower-ise signature component
    * Moves disabled tests out of the project
    * Move styles into their own file
    * Increase signature size

--- src/components/delivery/sign-off/sign-off.service.js
+++ src/components/delivery/sign-off/sign-off.service.js
@@ -2,19 +2,9 @@
 
 angular.module('delivery')
   .service('signOffService', function (deliveryService) {
-
-    var _this = this;
-
-    _this.isValidSignature  = function(signature){
+    this.isValidSignature = function(dataURL) {
       //FIXME: find better signature verification technique e.g base64 the image data uri
-      return ((signature.$isEmpty === false) && (signature.dataUrl.length > 0));
-    };
-
-    _this.getDefaultSignature = function(){
-      return {
-        dataUrl: '',
-        signedOn: ''
-      };
+      return dataURL.length > 0;
     };
 
     /**
@@ -25,10 +15,12 @@ angular.module('delivery')
      * @param {Data-URI} signature - image string of the signature in data uri format.
      * @returns {$promise}
      */
-    _this.signOff = function(dd, facRnd, signature){
+    this.signOff = function(dd, facRnd, signature){
       facRnd = deliveryService.setSuccessStatus(facRnd);
-      facRnd.signature = signature;
-      facRnd.signature.signedAt = new Date().toJSON();
+      facRnd.signature = {
+        dataUrl: signature,
+        signedAt: new Date().toJSON()
+      };
       dd = deliveryService.updateFacilityRound(dd, facRnd);
       return deliveryService.save(dd);
     };

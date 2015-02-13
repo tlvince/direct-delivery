2da7b2f0c7935b1634c31a5e76e24a3fe8cbd425
Tom Vincent <git@tlvince.com>
2015-02-13 11:40:32 +0200

    Re-resolve dailySchedule in tabs
    Note, we shouldn't have to do this; resolving `dailySchedule` once in the parent
    should work. Penned for review in item:1212.
    
    Also remove hacky conditional templates, simplified with ngIf (I'd still like
    revisit it if the templates get larger).

--- src/app/home/packing/home-packing.states.js
+++ src/app/home/packing/home-packing.states.js
@@ -4,30 +4,18 @@ angular.module('home.packing')
   .config(function($stateProvider) {
     $stateProvider.state('home.packing', {
       url: 'daily-packing',
-      templateUrl: 'app/home/packing/home-packing.html',
-      onEnter: function($timeout, $state, dailySchedule) {
-        if (dailySchedule) {
-          // XXX: $timeout required to prevent circular references, see:
-          // angular-ui/ui-router#1169
-          return $timeout(function() {
-            $state.go('home.packing.hasPacking');
-          });
-        }
-        $timeout(function() {
-          $state.go('home.packing.noPacking');
-        });
-      }
-    })
-    .state('home.packing.noPacking', {
-      url: '',
-      templateUrl: 'components/packing-table/no-packing.html'
-    })
-    .state('home.packing.hasPacking', {
-      url: '',
       templateUrl: 'components/packing-table/packing-table.html',
       controller: 'PackingTableCtrl',
       controllerAs: 'packingTableCtrl',
       resolve: {
+        dailySchedule: function(log, scheduleService) {
+          function errorHandler(error) {
+            log.error('dailyScheduleRetrival', error);
+            return {};
+          }
+          return scheduleService.getDaySchedule()
+            .catch(errorHandler);
+        },
         productStorages: function(packingTableLegendService) {
           return packingTableLegendService.get();
         }
--- src/app/home/schedule/home-schedule.states.js
+++ src/app/home/schedule/home-schedule.states.js
@@ -4,25 +4,18 @@ angular.module('home.schedule')
   .config(function($stateProvider) {
     $stateProvider.state('home.schedule', {
       url: 'daily-schedule',
-      templateUrl: 'app/home/schedule/layout.html',
-      onEnter: function($state, $timeout, dailySchedule) {
-        if (dailySchedule) {
-          return $timeout(function() {
-            $state.go('home.schedule.hasSchedule');
-          });
-        }
-        $timeout(function() {
-          $state.go('home.schedule.noSchedule');
-        });
-      }
-    })
-    .state('home.schedule.noSchedule', {
-      url: '',
-      templateUrl: 'app/home/schedule/no-schedule.html'
-    })
-    .state('home.schedule.hasSchedule', {
       templateUrl: 'app/home/schedule/home-schedule.html',
       controller: 'HomeScheduleCtrl',
-      controllerAs: 'homeScheduleCtrl'
+      controllerAs: 'homeScheduleCtrl',
+      resolve: {
+        dailySchedule: function(log, scheduleService) {
+          function errorHandler(error) {
+            log.error('dailyScheduleRetrival', error);
+            return {};
+          }
+          return scheduleService.getDaySchedule()
+            .catch(errorHandler);
+        }
+      }
     });
   });

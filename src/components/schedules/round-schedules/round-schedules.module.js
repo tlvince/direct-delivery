fe669fa384ef065ff6fa86e111ec22ee97f3aa90
femi oni <freefony@gmail.com>
2015-02-16 18:43:53 +0100

    add spies on dependencies

--- src/components/schedules/round-schedules/round-schedules.module.js
+++ src/components/schedules/round-schedules/round-schedules.module.js
@@ -1,4 +1,8 @@
 /**
  * Created by ehealthafrica on 1/13/15.
  */
-angular.module('schedules.round', []);
+angular.module('schedules.round', [
+  'ui.router',
+  'db',
+  'utility.pouchdb'
+]);
--- src/components/schedules/round-schedules/schedules.round.service.spec.js
+++ src/components/schedules/round-schedules/schedules.round.service.spec.js
@@ -2,17 +2,32 @@
  * Created by ehealthafrica on 2/13/15.
  */
 
-describes('schedule round service', function(){
+describe('schedule round service', function(){
 
-  var scheduleRoundService;
+  var scheduleRoundService, dbService, pouchUtil;
 
-  beforeEach(function(){
-    module('scheules.round');
+  beforeEach(module('schedules.round', 'db', 'utility.pouchdb'));
 
-    inject(function(_scheduleRoundService_, _dbService_, _pouchUtil_){
-      scheduleRoundService = _scheduleRoundService_;
-    })
-  })
+  beforeEach(inject(function(_scheduleRoundService_, _dbService_, _pouchUtil_){
+    scheduleRoundService = _scheduleRoundService_;
+    dbService = _dbService_;
+    pouchUtil = _pouchUtil_;
 
-  it('should')
+    spyOn(dbService, 'getView').and.callThrough();
+    spyOn(pouchUtil, 'pluckDocs').and.returnValue([{}]);
+  }));
+
+  it('should have get() method', function(){
+    expect(scheduleRoundService.get).toBeDefined();
+
+  });
+  it('should call dbService.getView()', function(){
+    scheduleRoundService.get();
+    expect(dbService.getView).toHaveBeenCalled();
+  });
+
+  it('should have getSortedByDate() method', function(){
+    expect(scheduleRoundService.getSortedByDate).toBeDefined();
+  });
+
 });

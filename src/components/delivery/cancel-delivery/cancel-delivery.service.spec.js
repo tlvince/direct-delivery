074436ecc89b802d98236e8086e6575050dcb389
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-02-24 11:34:00 +0100

    update validation logic for cancel reason === FAILED_OTHER or CANCELED_OTHER ref item:1250

--- src/components/delivery/cancel-delivery/cancel-delivery.service.spec.js
+++ src/components/delivery/cancel-delivery/cancel-delivery.service.spec.js
@@ -4,12 +4,19 @@ describe('cancelDeliveryService', function() {
   beforeEach(module('delivery', 'deliveryMock'));
 
   var cancelDeliveryService, cancelReport, deliveryStatus;
+  var facRnd;
 
   beforeEach(inject(function(_cancelDeliveryService_, _dailyDeliveryMock_, _DELIVERY_STATUS_) {
     cancelDeliveryService = _cancelDeliveryService_;
     cancelReport = cancelDeliveryService.getDefaultCancelReport();
     deliveryStatus = _DELIVERY_STATUS_;
+    facRnd = _dailyDeliveryMock_.facilityRounds[0];
   }));
+
+
+  afterEach(function(){
+    facRnd.status = deliveryStatus.UPCOMING_FIRST;
+  });
 
   it('should create and return default cancel report', function() {
     var cancelReport = cancelDeliveryService.getDefaultCancelReport();
@@ -27,16 +34,6 @@ describe('cancelDeliveryService', function() {
   });
 
   describe('validateCancelReport()', function(){
-
-    var facRnd;
-
-    beforeEach(inject(function(_dailyDeliveryMock_) {
-      facRnd = _dailyDeliveryMock_.facilityRounds[0];
-    }));
-
-    afterEach(function(){
-      facRnd.status = deliveryStatus.UPCOMING_FIRST;
-    });
 
     it('Should return TRUE if status equals DELIVERY_STATUS.FAILED_CCE', function(){
       expect(cancelDeliveryService.validateCancelReport(facRnd)).toBeFalsy();
@@ -101,6 +98,114 @@ describe('cancelDeliveryService', function() {
       expect(cancelDeliveryService.validateCancelReport(facRnd)).toBeTruthy();
       facRnd.status = deliveryStatus.SUCCESS_SECOND;
       expect(cancelDeliveryService.validateCancelReport(facRnd)).toBeFalsy();
+    });
+
+  });
+
+  describe('isOthers', function(){
+
+    it('Should return True if status equals DELIVERY_STATUS.FAILED_OTHER', function(){
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+      facRnd.status = deliveryStatus.FAILED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+    });
+
+    it('Should return True if status equals DELIVERY_STATUS.CANCELED_OTHER', function() {
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+      facRnd.status = deliveryStatus.CANCELED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+    });
+
+    it('Should return False if status is SUCCESS_FIRST', function(){
+      facRnd.status = deliveryStatus.CANCELED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+      facRnd.status = deliveryStatus.SUCCESS_FIRST;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+    });
+
+    it('Should return False if status is SUCCESS_SECOND', function(){
+      facRnd.status = deliveryStatus.CANCELED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+      facRnd.status = deliveryStatus.SUCCESS_SECOND;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+    });
+
+    it('Should return False if status is UPCOMING_FIRST', function(){
+      facRnd.status = deliveryStatus.CANCELED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+      facRnd.status = deliveryStatus.UPCOMING_FIRST;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+    });
+
+    it('Should return False if status is UPCOMING_SECOND', function(){
+      facRnd.status = deliveryStatus.CANCELED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+      facRnd.status = deliveryStatus.UPCOMING_SECOND;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+    });
+
+    it('Should return False if status is CANCELED_STAFF', function(){
+      facRnd.status = deliveryStatus.CANCELED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+      facRnd.status = deliveryStatus.CANCELED_STAFF;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+    });
+
+    it('Should return False if status is FAILED_STAFF', function(){
+      facRnd.status = deliveryStatus.CANCELED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+      facRnd.status = deliveryStatus.FAILED_STAFF;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+    });
+
+    it('Should return False if status is FAILED_CCE', function(){
+      facRnd.status = deliveryStatus.CANCELED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+      facRnd.status = deliveryStatus.FAILED_CCE;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+    });
+
+    it('Should return False if status is CANCELED_CCE', function(){
+      facRnd.status = deliveryStatus.CANCELED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+      facRnd.status = deliveryStatus.CANCELED_CCE;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+    });
+
+  });
+
+  describe('isValidOthers', function(){
+
+    it('Should return True if isOthers() is True and cancelReport.note is not empty string', function(){
+      expect(cancelDeliveryService.isValidOthers(facRnd)).toBeFalsy();
+      facRnd.cancelReport.note = 'test comment';
+      facRnd.status = deliveryStatus.FAILED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+      expect(cancelDeliveryService.isValidOthers(facRnd)).toBeTruthy();
+    });
+
+    it('Should return False if isOthers()  is true and cancelReport.note is EMPTY string', function(){
+      expect(cancelDeliveryService.isValidOthers(facRnd)).toBeFalsy();
+      facRnd.cancelReport.note = '';
+      facRnd.status = deliveryStatus.FAILED_OTHER;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeTruthy();
+      expect(cancelDeliveryService.isValidOthers(facRnd)).toBeFalsy();
+    });
+
+    it('Should return False if isOthers()  is False and cancelReport.note is EMPTY string', function(){
+      expect(cancelDeliveryService.isValidOthers(facRnd)).toBeFalsy();
+      facRnd.cancelReport.note = '';
+      facRnd.status = deliveryStatus.FAILED_CCE;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+      expect(cancelDeliveryService.isValidOthers(facRnd)).toBeFalsy();
+    });
+
+    it('Should return False if isOthers()  is False and cancelReport.note is NOT EMPTY string', function(){
+      expect(cancelDeliveryService.isValidOthers(facRnd)).toBeFalsy();
+      facRnd.cancelReport.note = 'test comment';
+      facRnd.status = deliveryStatus.FAILED_CCE;
+      expect(cancelDeliveryService.isOthers(facRnd)).toBeFalsy();
+      expect(cancelDeliveryService.isValidOthers(facRnd)).toBeFalsy();
     });
 
   });

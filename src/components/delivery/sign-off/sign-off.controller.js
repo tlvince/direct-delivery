c0ed152e74e33e1d66c17d7de1700ba16b651f80
femi oni <freefony@gmail.com>
2015-02-25 12:19:30 +0100

    update signoff buttons

--- src/components/delivery/sign-off/sign-off.controller.js
+++ src/components/delivery/sign-off/sign-off.controller.js
@@ -1,4 +1,4 @@
-'use strict';
+  'use strict';
 
 angular.module('delivery')
   .controller('SignOffCtrl', function FacilityDeliveryCtrl($state, $scope, signOffService, log, deliveryService, dailyDelivery) {
@@ -6,19 +6,6 @@ angular.module('delivery')
     var parent = $scope.facDevCtrl;
     vm.facRnd = parent.facRnd;
     vm.dailyDelivery = dailyDelivery;
-
-    vm.accepted = false;
-    vm.accept = function() {
-      if (vm.signature.isEmpty()) {
-        return log.error('invalidSignature');
-      }
-
-      if (vm.accepted) {
-        vm.signature.clear();
-      }
-
-      vm.accepted = !vm.accepted;
-    };
 
     vm.submit = function() {
       var dataURL = vm.signature.toDataURL();

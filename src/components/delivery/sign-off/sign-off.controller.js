78c4cb80aa9619f954489ae73ce1d56f79c200bf
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-03-02 10:17:41 +0100

    add default signature object and update controller logic

--- src/components/delivery/sign-off/sign-off.controller.js
+++ src/components/delivery/sign-off/sign-off.controller.js
@@ -6,17 +6,24 @@ angular.module('delivery')
     var parent = $scope.facDevCtrl;
     vm.facRnd = parent.facRnd;
     vm.dailyDelivery = dailyDelivery;
+    vm.signature = {
+      toDataURL: function(){
+        return '';
+      },
+      isEmpty: function() {
+        return true;
+      }
+    };
+
+    vm.isValidSignature = function(){
+      return vm.signature.isEmpty();
+    };
 
     vm.submit = function() {
       var dataURL = vm.signature.toDataURL();
-      var valid = signOffService.isValidSignature(dataURL);
-      if (vm.signature.isEmpty()) {
+      if (vm.isValidSignature()) {
         return log.error('invalidSignature');
       }
-      if (!valid) {
-        return log.error('invalidSignature');
-      }
-
       signOffService.signOff(dailyDelivery, vm.facRnd, dataURL)
         .then(deliveryService.saved)
         .catch(deliveryService.failed);

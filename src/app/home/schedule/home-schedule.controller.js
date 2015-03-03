b0931461a956ba049f6f1d61555d01ca06134f1f
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-03-03 10:47:11 +0100

    color code round status according to google-sheet code. refs  item: 1271

--- src/app/home/schedule/home-schedule.controller.js
+++ src/app/home/schedule/home-schedule.controller.js
@@ -1,16 +1,18 @@
 'use strict';
 
 angular.module('home.schedule')
-  .controller('HomeScheduleCtrl', function(dailySchedule, $scope, $rootScope, SYNC_STATUS) {
+  .controller('HomeScheduleCtrl', function (dailySchedule, $scope, $rootScope, SYNC_STATUS, DELIVERY_STATUS) {
     var vm = this;
     vm.day = dailySchedule;
+    vm.STATUS = DELIVERY_STATUS;
+
     var removeListener = $rootScope.$on(SYNC_STATUS.COMPLETE, processEvent);
 
-    function processEvent(event, data){
+    function processEvent(event, data) {
       vm.day = data.dailySchedule;
     }
 
-    vm.showScheduleTable = function(){
+    vm.showScheduleTable = function () {
       return (vm.day && angular.isArray(vm.day.facilityRounds) && vm.day.facilityRounds.length > 0);
     };
 

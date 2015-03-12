bd9a3beda05987e7f14627b49d50c0e108207225
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-03-12 17:00:41 +0100

    add DD doc changes saved and not saved message. refs item:1292

--- src/components/log/messages/info.constants.js
+++ src/components/log/messages/info.constants.js
@@ -4,6 +4,10 @@ angular.module('log')
   .constant('INFO_MESSAGES', {
     dailyScheduleRetrival: {
       title: 'Daily schedule',
-      message: 'Daily schedule does not exist.'
+      message: 'Daily schedule does not exist'
+    },
+    dailyDeliverySaved: {
+      title: 'Daily delivery saved',
+      message: 'Daily delivery saved so you won\'t lose changes'
     }
   });
--- src/components/log/messages/warning.constants.js
+++ src/components/log/messages/warning.constants.js
@@ -2,4 +2,8 @@
 
 angular.module('log')
   .constant('WARNING_MESSAGES', {
+    dailyDeliveryUpdateNotSaved: {
+      title: 'Daily delivery not saved',
+      message: 'Daily delivery changes, if any, was not saved'
+    }
   });

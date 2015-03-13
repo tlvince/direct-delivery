4c3edf523e72dfc986dba85b4c3844372562789d
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-03-13 12:07:06 +0100

    update sym-link path.

--- gulp/cordova/move.js
+++ gulp/cordova/move.js
@@ -11,7 +11,7 @@ var common = require('../../gulp/common');
 
 function formatAPKPaths(pkg, cb) {
   var apkPath = {
-    from: '../cordova/platforms/android/out/DirectDelivery',
+    from: '../cordova/platforms/android/bin/DirectDelivery',
     to: 'build/' + pkg.name
   };
   if (argv.release || common.build.release) {

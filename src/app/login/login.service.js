4f2e85f1091af040bec417c93f8521efc68f7258
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-03-24 11:56:41 +0100

    update log out logic

--- src/app/login/login.service.js
+++ src/app/login/login.service.js
@@ -7,18 +7,4 @@ angular.module('login')
       return AuthService.login(username, password);
     };
 
-    this.logout = function() {
-      function login() {
-        return $state.go('login');
-      }
-
-      function handleError(err) {
-        log.warn('failedServerLogout', msg);
-        return $state.go('login');
-      }
-
-      return AuthService.logout()
-        .then(login)
-        .catch(handleError);
-    };
   });

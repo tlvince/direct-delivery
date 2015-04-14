31d4d738e5cfaa73545336ceee49a361eac0829b
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-04-14 17:13:05 +0100

    minor changes to facility kpi state and view

--- src/app/kpi/facility-kpi.state.js
+++ src/app/kpi/facility-kpi.state.js
@@ -2,15 +2,16 @@
 
 angular
   .module('kpi')
-  .config(function($stateProvider) {
-    $stateProvider.state('facilityKPI', {
-      parent: 'index',
-      url: 'facility-kpi',
-      templateUrl: 'app/kpi/facility-kpi.html',
-      controller: 'FacilityKPICtrl',
-      controllerAs: 'facKPICtrl',
-      data: {
-        label: 'Facility KPI'
-      }
-    });
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('facilityKPI', {
+        parent: 'index',
+        url: 'facility-kpi',
+        templateUrl: 'app/kpi/facility-kpi.html',
+        controller: 'FacilityKPICtrl',
+        controllerAs: 'facKPICtrl',
+        data: {
+          label: 'Facility KPI'
+        }
+      });
   });

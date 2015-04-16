fc1d9d67d6cf1e35cf2a08b735d8575f19958a77
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-04-16 12:01:02 +0100

    add more test to facilityKPIService test suite

--- src/app/kpi/facility-kpi.controller.spec.js
+++ src/app/kpi/facility-kpi.controller.spec.js
@@ -6,21 +6,34 @@ describe('FacilityKPICtrl', function() {
   beforeEach(module('kpi'));
 
   var FacilityKPICtrl;
+  var facilityKPIService;
+  var AuthService;
+  var utility;
+  var driverId = 'test@user.com';
 
   beforeEach(inject(function($controller, _facilityKPIService_, _AuthService_, _scheduleService_,
                              _utility_, _deliveryService_, _log_, _$state_) {
 
-    _AuthService_.setCurrentUser({ name: 'test@user.com' });
+    facilityKPIService = _facilityKPIService_;
+    AuthService = _AuthService_;
+    utility = _utility_;
+    AuthService.setCurrentUser({ name: driverId });
 
     FacilityKPICtrl = $controller('FacilityKPICtrl', {
-      facilityKPIService: _facilityKPIService_,
-      AuthService: _AuthService_,
-      utility: _utility_,
+      facilityKPIService: facilityKPIService,
+      AuthService: AuthService,
+      utility: utility,
       scheduleService: _scheduleService_,
       deliveryService: _deliveryService_,
       log: _log_,
       $state: _$state_
     });
+
+    spyOn(facilityKPIService, 'isValidKPI').and.callThrough();
+    spyOn(facilityKPIService, 'getFacilityKPIFrom').and.callThrough();
+    spyOn(facilityKPIService, 'getFromById').and.callThrough();
+    spyOn(facilityKPIService, 'loadFacilities').and.callThrough();
+    spyOn(facilityKPIService, 'isValidFacilityKPI').and.callThrough();
 
   }));
 
@@ -37,6 +50,43 @@ describe('FacilityKPICtrl', function() {
     });
   });
 
+  describe('isValidKPI()', function() {
+    it('Should call facilityKPIService.isValidKPI()', function(){
+      expect(facilityKPIService.isValidKPI).not.toHaveBeenCalled();
+      FacilityKPICtrl.isValidKPI();
+      expect(facilityKPIService.isValidKPI).toHaveBeenCalled();
+    });
+  });
+
+  describe('loadFacilityKPI()', function(){
+    it('Should set FacilityKPICtrl.selectedLoadKPI to TRUE', function() {
+      expect(FacilityKPICtrl.selectedLoadKPI).not.toBeTruthy();
+      FacilityKPICtrl.loadFacilityKPI();
+      expect(FacilityKPICtrl.selectedLoadKPI).toBeTruthy();
+    });
+
+    it('Should call facilityKPIService.getFacilityKPIFrom()', function(){
+      expect(facilityKPIService.getFacilityKPIFrom).not.toHaveBeenCalled();
+      FacilityKPICtrl.loadFacilityKPI();
+      expect(facilityKPIService.getFacilityKPIFrom).toHaveBeenCalled();
+    });
+
+    it('Should call facilityKPIService.getFromById()', function(){
+      expect(facilityKPIService.getFromById).not.toHaveBeenCalled();
+      FacilityKPICtrl.loadFacilityKPI();
+      expect(facilityKPIService.getFromById).toHaveBeenCalled();
+    });
+  });
+
+  describe('loadFacilities()', function() {
+    it('Should call facilityKPIService.loadFacilities() with expected parameters', function() {
+      var expectedDriverId = driverId;
+      var expectedDate = utility.extractDate(new Date());
+      expect(facilityKPIService.loadFacilities).not.toHaveBeenCalled();
+      FacilityKPICtrl.loadFacilities();
+      expect(facilityKPIService.loadFacilities).toHaveBeenCalledWith(expectedDriverId, expectedDate);
+    });
+  });
 
 });
 

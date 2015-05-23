0d4a97be213682dab7beb48c13827caa0f2c1609
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-05-23 12:49:12 +0100

    remember last opened round tab

--- src/components/schedules/round-schedules/round-schedules.controller.js
+++ src/components/schedules/round-schedules/round-schedules.controller.js
@@ -3,38 +3,47 @@
  */
 
 angular.module('schedules.round')
-  .controller('SchedulesRoundCtrl', function(scheduleService, scheduleRoundService, rounds,
-                                             utility, DELIVERY_STATUS, deliveryService){
-    var _this = this;
+    .controller('SchedulesRoundCtrl', function (scheduleService, scheduleRoundService, rounds,
+                                                utility, DELIVERY_STATUS, deliveryService, $state) {
+      var _this = this;
 
-    _this.STATUS = DELIVERY_STATUS;
-    _this.rounds = rounds;
-
-    _this.getColorCode = function(status, ccsClass) {
-      return deliveryService.getStatusColor(status, ccsClass);
-    };
-
-    _this.formatDate = function(date){
-      if((new Date(date)).toString() !== 'Invalid Date'){
-        return utility.formatDate(date, "dd, MMM yyyy");
+      function openRoundTab() {
+        if($state.params.roundId) {
+          _this.toggleDisplay($state.params.roundId);
+        }
       }
-      return 'N/A';
-    };
 
-    _this.displayRound = null;
-    _this.roundToDisplay = null;
-    _this.showDisplayRound = function(roundUUID){
-      scheduleService.getByRound(roundUUID)
-        .then(function(res){
-          _this.displayRound = res;
-        });
-    };
-    _this.toggleDisplay = function(roundId){
-      if(roundId === _this.roundToDisplay){
-        _this.roundToDisplay = null;
-      }else{
-        _this.roundToDisplay = roundId;
-        _this.showDisplayRound(roundId);
+      _this.STATUS = DELIVERY_STATUS;
+      _this.rounds = rounds;
+
+      _this.getColorCode = function (status, ccsClass) {
+        return deliveryService.getStatusColor(status, ccsClass);
+      };
+
+      _this.formatDate = function (date) {
+        if ((new Date(date)).toString() !== 'Invalid Date') {
+          return utility.formatDate(date, "dd, MMM yyyy");
+        }
+        return 'N/A';
+      };
+
+      _this.displayRound = null;
+      _this.roundToDisplay = null;
+      _this.showDisplayRound = function (roundUUID) {
+        scheduleService.getByRound(roundUUID)
+            .then(function (res) {
+              _this.displayRound = res;
+            });
+      };
+
+      _this.toggleDisplay = function (roundId) {
+        if (roundId === _this.roundToDisplay) {
+          _this.roundToDisplay = null;
+        } else {
+          _this.roundToDisplay = roundId;
+          _this.showDisplayRound(roundId);
+        }
       }
-    }
-  });
+
+      openRoundTab();
+    });

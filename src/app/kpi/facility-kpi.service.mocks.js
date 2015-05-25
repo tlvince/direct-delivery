5342890f5d4b0d578c096ca7c742ce3c9b7f8b78
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-05-25 14:45:45 +0100

    change kpi mock to new structure

--- src/app/kpi/facility-kpi.service.mocks.js
+++ src/app/kpi/facility-kpi.service.mocks.js
@@ -3,20 +3,56 @@
 angular
   .module('facilityKPIMocks', [])
   .constant('facilityKPIMock', {
-    "outreachSessions": 0,
-    "notes": "",
-    "antigensKPI": [
-      {
-        "productID": "BCG",
-        "noImmuized": 0
-      },
-      {
-        "productID": "Men-A",
-        "noImmuized": 0
-      },
-      {
-        "productID": "OPV",
-        "noImmuized": 0
+      "_id": "2164951f4fdd7e506c000a3dc4078ffb",
+      "_rev": "2-8ad7f15835a637ad5b3bd1c1cdfd0627",
+      "outreachSessions": "",
+      "notes": "",
+      "doc_type": "kpi",
+      "date": "2015-05-27",
+      "originRow": "163",
+      "deliveryRoundID": "KN-21-2015",
+      "driverID": "abdullahi@example.com",
+      "version": "2.0.0",
+      "worksheetId": "sheetId",
+      "spreadsheetId": "uuid",
+      "importedAt": "2015-05-16T12:03:26.801Z",
+      "antigensKPI": [
+        {
+          "productID": "BCG",
+          "noImmuized": 0
+        },
+        {
+          "productID": "MV",
+          "noImmuized": 0
+        },
+        {
+          "productID": "OPV",
+          "noImmuized": 0
+        },
+        {
+          "productID": "IPV",
+          "noImmuized": 0
+        },
+        {
+          "productID": "TT",
+          "noImmuized": 0
+        },
+        {
+          "productID": "Penta",
+          "noImmuized": 0
+        },
+        {
+          "productID": "HBV",
+          "noImmuized": 0
+        }
+      ],
+      "facility": {
+        "zone": "test zone",
+        "lga": "test lga",
+        "ward": "test ward",
+        "name": "Test Health Post",
+        "id": "NZ TSA 101",
+        "contact": "Test Person",
+        "phoneNo": 80232
       }
-    ]
-  });
+    });

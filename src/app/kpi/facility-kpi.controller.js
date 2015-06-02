7254c3d45e29f9246600e4ff1141d9f56e79c833
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-02 16:55:38 +0100

    save skip kpi note

--- src/app/kpi/facility-kpi.controller.js
+++ src/app/kpi/facility-kpi.controller.js
@@ -1,122 +1,133 @@
 'use strict';
 
 angular.module('kpi')
-  .controller('FacilityKPICtrl', function (facilityKPIService, utility, AuthService, log, $state) {
+		.controller('FacilityKPICtrl', function (facilityKPIService, utility, AuthService, log, $state) {
 
-    var vm = this;
-    var driverId = AuthService.currentUser.name;
-    vm.kpiList = [];
-    var shouldShowKPI = false;
-    var shouldSkipKPI = false;
+			var vm = this;
+			var driverId = AuthService.currentUser.name;
+			vm.kpiList = [];
+			var shouldShowKPI = false;
+			var shouldSkipKPI = false;
 
-    function setKPIList(kpiDocs) {
-      vm.kpiList = kpiDocs;
-    }
+			function setKPIList(kpiDocs) {
+				vm.kpiList = kpiDocs;
+			}
 
-    vm.getByDriverAndDate = function(){
-      vm.facilityKPI = null;
-      vm.selectedFacilityId = '';
-      vm.selectedLoadKPI = false;
-      vm.selectedFacility = null;
-      facilityKPIService.getBy(driverId, vm.selectedDate)
-        .then(setKPIList)
-        .catch(function() {
-            setKPIList([]);
-        });
-    };
+			vm.getByDriverAndDate = function () {
+				vm.facilityKPI = null;
+				vm.selectedFacilityId = '';
+				vm.selectedLoadKPI = false;
+				vm.selectedFacility = null;
+				facilityKPIService.getBy(driverId, vm.selectedDate)
+						.then(setKPIList)
+						.catch(function () {
+							setKPIList([]);
+						});
+			};
 
-    vm.getFacilityKPI = function() {
-      vm.selectedLoadKPI = true;
-      vm.facilityKPI = facilityKPIService.getKPIFromBy(vm.kpiList, vm.selectedFacilityId);
-      if(vm.facilityKPI && vm.facilityKPI.facility){
-        vm.selectedFacility = vm.facilityKPI.facility;
-        return;
-      }
-      vm.selectedFacility = null;
-      shouldSkipKPI = false;
-      shouldShowKPI = false;
-    };
+			vm.getFacilityKPI = function () {
+				vm.selectedLoadKPI = true;
+				vm.facilityKPI = facilityKPIService.getKPIFromBy(vm.kpiList, vm.selectedFacilityId);
+				if (vm.facilityKPI && vm.facilityKPI.facility) {
+					vm.selectedFacility = vm.facilityKPI.facility;
+					return;
+				}
+				vm.selectedFacility = null;
+				shouldSkipKPI = false;
+				shouldShowKPI = false;
+			};
 
-    vm.isValidKPI = function(){
-      return facilityKPIService.isValidKPI(vm.facilityKPI);
-    };
+			vm.isValidKPI = function () {
+				return facilityKPIService.isValidKPI(vm.facilityKPI);
+			};
 
-    function clearValidationError(){
-      vm.kpiError = {
-        outreach: false,
-        antigensNoImmunized: {}
-      };
-    }
+			function clearValidationError() {
+				vm.kpiError = {
+					outreach: false,
+					antigensNoImmunized: {}
+				};
+			}
 
-    function initialize() {
-      vm.selectedLoadKPI = false;
-      vm.previewKPI = false;
-      vm.facilityKPI = null;
-      vm.selectedFacilityId = null;
-      vm.selectedDate = utility.extractDate(new Date());
-      vm.getByDriverAndDate();
-      setKPIList([]);
-      clearValidationError();
-    }
+			function initialize() {
+				vm.selectedLoadKPI = false;
+				vm.previewKPI = false;
+				vm.facilityKPI = null;
+				vm.selectedFacilityId = null;
+				vm.selectedDate = utility.extractDate(new Date());
+				vm.getByDriverAndDate();
+				setKPIList([]);
+				clearValidationError();
+			}
 
-    initialize();
+			initialize();
 
-    vm.togglePreview = function(){
-      vm.previewKPI = !vm.previewKPI;
-    };
+			vm.togglePreview = function () {
+				vm.previewKPI = !vm.previewKPI;
+			};
 
-    vm.isValidOutreach = function(){
-      vm.kpiError.outreach =  !facilityKPIService.isValidOutreach(vm.facilityKPI.outreachSessions);
-    };
+			vm.isValidOutreach = function () {
+				vm.kpiError.outreach = !facilityKPIService.isValidOutreach(vm.facilityKPI.outreachSessions);
+			};
 
-    vm.preview = function(){
-      var res = facilityKPIService.isValidFacilityKPI(vm.facilityKPI);
-      if(res === true){
-        clearValidationError();
-        vm.togglePreview();
-      }else{
-        vm.kpiError = res;
-      }
-    };
+			vm.preview = function () {
+				var res = facilityKPIService.isValidFacilityKPI(vm.facilityKPI);
+				if (res === true) {
+					clearValidationError();
+					vm.togglePreview();
+				} else {
+					vm.kpiError = res;
+				}
+			};
 
-    vm.isInvalid = function(index){
-      var antigen = vm.facilityKPI.antigensKPI[index];
-      vm.kpiError.antigensNoImmunized[index] = !angular.isNumber(antigen.noImmunized);
-    };
+			vm.isInvalid = function (index) {
+				var antigen = vm.facilityKPI.antigensKPI[index];
+				vm.kpiError.antigensNoImmunized[index] = !angular.isNumber(antigen.noImmunized);
+			};
 
-    vm.save = function() {
-      var kpiDoc = angular.copy(vm.facilityKPI);
-      facilityKPIService.save(kpiDoc)
-          .then(function() {
-            log.success('kpiSaved');
-            $state.go('home.schedule');
-          })
-          .catch(function(err) {
-            log.error('saveKPIFail', err);
-          });
-    };
+			vm.save = function () {
+				var kpiDoc = angular.copy(vm.facilityKPI);
+				facilityKPIService.save(kpiDoc)
+						.then(function () {
+							log.success('kpiSaved');
+							$state.go('home.schedule');
+						})
+						.catch(function (err) {
+							log.error('saveKPIFail', err);
+						});
+			};
 
-    vm.shouldAddKPI = function() {
-      return shouldShowKPI;
-    };
+			vm.skipKPI = function () {
+				var kpiDoc = angular.copy(vm.facilityKPI);
+				facilityKPIService.save(kpiDoc)
+						.then(function () {
+							log.success('kpiSaved');
+							$state.go('home.schedule');
+						})
+						.catch(function (err) {
+							log.error('saveKPIFail', err);
+						});
+			};
 
-    vm.shouldSkipKPI = function() {
-      return shouldSkipKPI;
-    };
+			vm.shouldAddKPI = function () {
+				return shouldShowKPI;
+			};
 
-    vm.showKPIForm = function() {
-      shouldShowKPI = true;
-      shouldSkipKPI = false;
-    };
+			vm.shouldSkipKPI = function () {
+				return shouldSkipKPI;
+			};
 
-    vm.showSkipForm = function() {
-      shouldShowKPI = false;
-      shouldSkipKPI = true;
-    };
+			vm.showKPIForm = function () {
+				shouldShowKPI = true;
+				shouldSkipKPI = false;
+			};
 
-    vm.hideOptions = function(){
-      var isFacilityNotSelected = (vm.selectedFacilityId === '' || vm.selectedFacility === null);
-      return isFacilityNotSelected;
-    };
+			vm.showSkipForm = function () {
+				shouldShowKPI = false;
+				shouldSkipKPI = true;
+			};
 
-  });
+			vm.hideOptions = function () {
+				return (vm.selectedFacilityId === '' || vm.selectedFacility === null);
+			};
+
+		});

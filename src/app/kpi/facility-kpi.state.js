9bfa516c383325c80d5c5dcc3b66c90dc0fdf303
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-03 17:04:25 +0100

    create Facility KPI list view state.

--- src/app/kpi/facility-kpi.state.js
+++ src/app/kpi/facility-kpi.state.js
@@ -1,17 +1,30 @@
 'use strict';
 
 angular
-  .module('kpi')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('facilityKPI', {
-        parent: 'index',
-        url: 'facility-kpi',
-        templateUrl: 'app/kpi/facility-kpi.html',
-        controller: 'FacilityKPICtrl',
-        controllerAs: 'facKPICtrl',
-        data: {
-          label: 'Facility KPI'
-        }
-      });
-  });
+		.module('kpi')
+		.config(function ($stateProvider) {
+			$stateProvider
+					.state('facilityKPI', {
+						parent: 'index',
+						url: 'facility-kpi',
+						templateUrl: 'app/kpi/facility-kpi.html',
+						controller: 'FacilityKPICtrl',
+						controllerAs: 'facKPICtrl'
+					})
+					.state('facilityKPIListView', {
+						parent: 'index',
+						url: 'facility-kpi-list-view',
+						templateUrl: 'app/kpi/list-view/facility-kpi-list.html',
+						controller: 'FacilityKPIListCtrl',
+						controllerAs: 'facKPIListCtrl',
+						data: {
+							label: 'Facility KPI'
+						},
+						resolve: {
+							kpiSorted: function(facilityKPIService, AuthService) {
+								var driverId = AuthService.currentUser.name;
+								return facilityKPIService.getByDriverSorted(driverId);
+							}
+						}
+					});
+		});

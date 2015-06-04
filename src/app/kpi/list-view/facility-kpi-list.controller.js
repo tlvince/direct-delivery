c32898009c8a50fd597a3eeacc342a21219277c7
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-04 15:25:02 +0100

    add showTable() and test specs for FacilityKPIListView

--- src/app/kpi/list-view/facility-kpi-list.controller.js
+++ src/app/kpi/list-view/facility-kpi-list.controller.js
@@ -1,13 +1,22 @@
 'use strict';
 
-angular.module('kpi')
+angular
+		.module('kpi')
 		.controller('FacilityKPIListCtrl', function (kpiSorted, utility, facilityKPIService) {
+
 			var vm = this;
 			vm.kpiList = kpiSorted;
-			vm.formatDate = utility.formatDate;
+
+			vm.formatDate = function(date) {
+				return utility.formatDate(date);
+			};
 
 			vm.isComplete = function(kpiDoc){
 				return (facilityKPIService.isValidFacilityKPI(kpiDoc) === true);
+			};
+
+			vm.showTable = function(){
+				return (angular.isArray(vm.kpiList) && vm.kpiList.length > 0);
 			};
 
 		});

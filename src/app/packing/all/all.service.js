cc5de354fbf4617f9f53a590036cab0b6728bc05
Tom Vincent <git@tlvince.com>
2015-11-06 12:30:44 +0000

    refactor(packing): remove unused reduce

--- src/app/packing/all/all.service.js
+++ src/app/packing/all/all.service.js
@@ -4,7 +4,6 @@ angular.module('packing.all')
   .service('packingAllService', function(AuthService, pouchUtil, dbService) {
     this.all = function() {
       var params = pouchUtil.key(AuthService.currentUser.name);
-      params.reduce = false;
       return dbService.getView('packing/by-driver', params)
         .then(pouchUtil.pluckValues);
     };

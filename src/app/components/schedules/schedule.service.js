a28416be6118f66779841bc070ca468ac7a06742
Tom Vincent <git@tlvince.com>
2015-11-26 12:59:03 +0100

    style(schedule): fix lint warnings

--- src/app/components/schedules/schedule.service.js
+++ src/app/components/schedules/schedule.service.js
@@ -5,6 +5,33 @@
 
 angular.module('schedules')
   .service('scheduleService', function(AuthService, dbService, pouchUtil, utility) {
+    function flattenRoundsList(dailyDelivery) {
+      var flattened = [];
+      var rounds = angular.copy(dailyDelivery.facilityRounds);
+      delete dailyDelivery.facilityRounds;
+      var i = rounds.length;
+      while (i--) {
+        flattened.push(angular.extend(rounds[i], dailyDelivery));
+      }
+      return flattened;
+    }
+
+    function formatRounds(list) {
+      var formattedList = [];
+      var i = list.length;
+      while (i--) {
+        if (list[i].hasOwnProperty('facilityRounds')) {
+          formattedList = formattedList.concat(flattenRoundsList(list[i]));
+        } else {
+          formattedList.push(list[i]);
+        }
+      }
+
+      formattedList.sort(function (a, b) {
+        return a.drop - b.drop;
+      });
+      return formattedList;
+    }
 
     this.get = function(view, key) {
 
@@ -33,33 +60,4 @@ angular.module('schedules')
         .then(pouchUtil.pluckDocs)
         .then(pouchUtil.rejectIfEmpty);
     };
-
-    function formatRounds(list) {
-      var formattedList = [];
-      var i = list.length;
-      while (i--) {
-        if (list[i].hasOwnProperty('facilityRounds')) {
-          formattedList = formattedList.concat(flattenRoundsList(list[i]))
-        } else {
-          formattedList.push(list[i]);
-        }
-      }
-
-      formattedList.sort(function (a, b) {
-        return a.drop - b.drop;
-      });
-      return formattedList;
-    }
-
-    function flattenRoundsList(dailyDelivery) {
-      var flattened = [];
-      var rounds = angular.copy(dailyDelivery.facilityRounds);
-      delete dailyDelivery['facilityRounds'];
-      var i = rounds.length;
-      while (i--) {
-        flattened.push(angular.extend(rounds[i], dailyDelivery));
-      }
-      return flattened;
-    }
-
   });

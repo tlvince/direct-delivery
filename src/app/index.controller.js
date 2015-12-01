ee367d46970f0eb5652f26f507cc388449f3ff0f
Tom Vincent <git@tlvince.com>
2015-12-01 16:56:41 +0100

    fix: log state change errors
    Closes #274.

--- src/app/index.controller.js
+++ src/app/index.controller.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('directDelivery')
-  .controller('IndexCtrl', function($rootScope, log, $state, hasCompleteDesignDocs) {
+  .controller('IndexCtrl', function(
+    $log,
+    $state,
+    $rootScope,
+    log,
+    hasCompleteDesignDocs
+  ) {
 
-    function stateChangeError(event) {
+    function stateChangeError(event, toState, toParams, fromState, fromParams, err) {
       log.error('stateChangeError', event);
+      $log.error(err);
     }
 
     $rootScope.$on('$stateChangeError', stateChangeError);
